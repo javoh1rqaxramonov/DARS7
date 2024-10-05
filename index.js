5eace904c8608d305fba6ada159dd2a002ed66e1
javoh1rqaxramonov <ubotirov22@.com>
2024-10-05 16:50:52 +0500

    finished

---
+++ index.js
@@ -0,0 +1,187 @@
+// Math
+
+// 1-masala 
+// let a = 100;
+// console.log(Math.trunc(Math.random(a)*100));   
+
+// 2-masala
+// let a = +prompt("sonni kiriting");
+// console.log(Math.sqrt(a));
+
+// 3-masala 
+// let son1 = +prompt("1-sonni kiritng");
+// let son2 = +prompt("2-sonni kiritng");
+// let son3 = +prompt("3-sonni kiritng");
+// let son4 = +prompt("4-sonni kiritng");
+// let son5 = +prompt("5-sonni kiritng");
+
+// let max = Math.max(son1,son2,son3,son4);
+// let min = Math.min(son1,son2,son3,son4);
+
+// console.log(max);
+// console.log(min);
+
+// 4-masala 
+// let a =4.5;
+// console.log(Math.floor(a));
+// console.log(Math.ceil(a));
+ 
+// 5-masala 
+// let r=+prompt("radiusdi kiriting");
+// let c = Math.trunc(2*Math.PI * r);
+// console.log(c);
+
+
+// ternary
+
+// 1-masala 
+// let a = +prompt("sonni kiriting");
+// a > 0 ? console.log("musbat son"):console.log("Manfiy son");
+
+// 2-masala 
+// let a = +prompt("yoshingizni kiriting");
+// a >= 18 ? console.log("ruhsat etilgan"):console.log("ruhsat etilmagan");
+
+// 3-masala 
+// let a = +prompt("sonni kiriting");
+// a %2==0 ? console.log("juft son"):console.log("toq son");
+
+// for sikl
+
+// 1-masala 
+// for (let i = 1; i <=20; i++) {
+//     if (i%2==0 && i<=20) {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// 2-masala 
+// let sum = 0 ;
+// for (let i = 1; i <=10; i++) {
+//     let a = +prompt("sonni kiriting");
+//     if (i>0 ) {
+//         sum+=i;   
+//     }
+    
+// }
+// console.log(sum);
+
+// 3-masala 
+// for (let i = 1; i <=100; i++) {
+//     if (i>=30 && i<=50) {
+//         console.log(i);
+        
+//     }
+    
+// }
+
+// 4-masala 
+// let counterMusbat = 0;
+// let counterManfiy = 0;
+// for (let i = 1; i <=15; i++) {
+//     let a = +prompt("sonni kiriting");
+//     if (a>=1) { 
+//         counterMusbat++;
+//     } else{
+//         counterManfiy++;
+//     }
+    
+// }
+// console.log(counterManfiy);
+// console.log(counterMusbat);
+
+// 6-masala
+// let sum = 0 ;
+// for (let i = 1; i <=50; i++) {
+//     if (i%2==1) {
+//         sum+=i;
+//     }
+    
+// }
+// console.log(sum);
+
+// 7-masala 
+// for (let i = 1; i <=5; i++) {
+//     let yosh = +prompt("yoshlar kiriting");
+//     if (yosh>=18) {
+//         console.log("voyaga yetgan");
+        
+//     } else {
+//         console.log("voyaga yetmagan");
+        
+//     }
+// }
+
+// 8-masala 
+// let counter = 0 ;
+// for (let i = 1; i <=100; i++) {
+//     if (i%2==0) {
+//         counter++;
+//     }
+    
+// }
+// console.log(counter);
+
+
+// 9-masala
+// let counter = 0;
+// for (let i = 1; i <=10; i++) {
+//     let son = +prompt("sonni kiriting");
+//     if (son>=5 && son<=10) {
+//         counter++;
+//     }
+// }
+// console.log(counter);
+
+
+// if (counter>0) {
+//     console.log("5 va 10 son orasida son bor");   
+// } else{
+//     console.log("5 va 10 son orasida son yoq");
+    
+// }
+
+// 10-masala 
+// for (let i = 1; i <=10; i++) {
+//     let son = +prompt("sonni kiriting");
+//     if (son>0){
+//         console.log(son);
+//     }
+// }
+
+// 11-masala
+// let counter = 0;
+// for (let i = 1; i <=15; i++) {
+//     let a = +prompt("sonni kiriting");
+//     if (a>0 || a%2==0 ) { 
+//         counter++;
+//     } 
+    
+// }
+// console.log(counter);
+
+// 12-masala 
+// for (let i = 1; i <=50; i++) {
+//     if (i%3==0) {
+//         console.log(i);
+        
+//     }    
+// }
+
+// 14-masala 
+// for (let i = 1; i <=15; i++) {
+//     let son = +prompt("sonni kiriting");
+//     if (son>=50){
+//         console.log(son);
+//     }
+// }
+
+// 15-masala 
+// for (let i = 1; i <=10; i++) {
+//     let son = +prompt("sonni kiriting");
+//     if (son===0){
+//         console.log("0 mavjut");
+//     }
+// }
